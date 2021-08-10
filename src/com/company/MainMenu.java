cb4c622e190c6a09739dac2828c7e2483e6e3686
AndreyFCH <86534492+AndreyFCH@users.noreply.github.com>
2021-08-09 18:08:30 -0600

    F

--- src/com/company/MainMenu.java
+++ src/com/company/MainMenu.java
@@ -21,28 +21,26 @@ public class MainMenu extends JFrame {
         this.setSize(400,300);
         this.setLocation(800,400);
 
+        Catalogo libro1 = new Catalogo("1010", "Don Quijote", "Rafael Garcia Marquez", "Patito",true);
+        Catalogo libro2 = new Catalogo("1011", "Harry Potter", "Mario Martinez", "Universal",true);
+        Catalogo libro3 = new Catalogo("1012", "Maze Runner", "Viviana Zeledon", "Alba",true);
+        Catalogo libro4 = new Catalogo("1013", "Sinsajo", "Pedro Fuente", "Critica",true);
+        Catalogo libro5 = new Catalogo("1014", "Biblioteca de los Muertos", "Carlos Mora", "Gredos",true);
+
+
+        Catalogo librosregistrados[] = new Catalogo[5];
+
+        librosregistrados[0] = libro1;
+        librosregistrados[1] = libro2;
+        librosregistrados[2] = libro3;
+        librosregistrados[3] = libro4;
+        librosregistrados[4] = libro5;
 
         btnBuscar.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String buscar = txtBuscar.getText();
                 String dispo = "";
-
-                Catalogo libro1 = new Catalogo("1010", "Don Quijote", "Rafael Garcia Marquez", "Patito",true);
-                Catalogo libro2 = new Catalogo("1011", "Harry Potter", "Mario Martinez", "Universal",true);
-                Catalogo libro3 = new Catalogo("1012", "Maze Runner", "Viviana Zeledon", "Alba",true);
-                Catalogo libro4 = new Catalogo("1013", "Sinsajo", "Pedro Fuente", "Critica",true);
-                Catalogo libro5 = new Catalogo("1014", "Biblioteca de los Muertos", "Carlos Mora", "Gredos",true);
-
-
-                Catalogo librosregistrados[] = new Catalogo[5];
-
-                librosregistrados[0] = libro1;
-                librosregistrados[1] = libro2;
-                librosregistrados[2] = libro3;
-                librosregistrados[3] = libro4;
-                librosregistrados[4] = libro5;
-
 
                 for (int i = 0; i < librosregistrados.length; i++){
                     if(buscar.equals(librosregistrados[i].codigoISBNLibro) || buscar.equals(librosregistrados[i].tituloLibro)){
@@ -54,13 +52,11 @@ public class MainMenu extends JFrame {
                             dispo += "No";
                         }
 
+
                         JOptionPane.showMessageDialog(null, "Editorial: " + librosregistrados[i].editoralLibro + "\n" + "Autor: " + librosregistrados[i].AutorLibro + "\n" + "Titulo: " + librosregistrados[i].tituloLibro + "\n" + "Codigo ISBN: " + librosregistrados[i].codigoISBNLibro + "\n" +"Disponibilidad: " + dispo  );
 
-
                     }
-
                 }
-
 
             }
         });
